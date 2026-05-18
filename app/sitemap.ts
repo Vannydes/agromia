61ef9178386d5d6fce0355bb79ce2fc8de249954
Vannydes <vanny.sto78@gmail.com>
2026-05-18 13:53:31 +0200

    Optimize sitemap structure

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,48 +1,37 @@
-import type { MetadataRoute } from 'next';
+import { MetadataRoute } from 'next';
 
 const baseUrl = 'https://agromia.vercel.app';
 
 const now = new Date();
 
-const pages: MetadataRoute.Sitemap = [
-  {
-    url: `${baseUrl}/`,
-    lastModified: now,
-    changeFrequency: 'daily',
-    priority: 1,
-  },
-  {
-    url: `${baseUrl}/login`,
-    lastModified: now,
-    changeFrequency: 'weekly',
-    priority: 0.7,
-  },
-  {
-    url: `${baseUrl}/register`,
-    lastModified: now,
-    changeFrequency: 'weekly',
-    priority: 0.7,
-  },
-  {
-    url: `${baseUrl}/add-crop`,
-    lastModified: now,
-    changeFrequency: 'weekly',
-    priority: 0.6,
-  },
-  {
-    url: `${baseUrl}/privacy-policy`,
-    lastModified: now,
-    changeFrequency: 'monthly',
-    priority: 0.5,
-  },
-  {
-    url: `${baseUrl}/terms-of-service`,
-    lastModified: now,
-    changeFrequency: 'monthly',
-    priority: 0.5,
-  },
-];
-
 export default function sitemap(): MetadataRoute.Sitemap {
-  return pages;
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: now,
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+
+    {
+      url: `${baseUrl}/demo`,
+      lastModified: now,
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: now,
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/terms-of-service`,
+      lastModified: now,
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
 }
